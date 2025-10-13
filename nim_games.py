a6b8862d28dbcbc5304195fa0453f2c5ab8bb4b0
Yassine <yassine.zriba@fms-ea.com>
2025-10-13 14:03:35 +0200

    Function to send player name

--- nim_games.py
+++ nim_games.py
@@ -4,3 +4,17 @@
 """
 Jeux  de Nim (variante simple et de Marienbad)
 """
+
+init_match = 21
+min_removed_pil = 1
+max_removed_pil = 4
+
+def name_of_player(player):
+
+	while True:
+		name = input("Enter your name: ").strip()
+		if name:
+			return name
+		print("Sorry, try again.")
+
+
